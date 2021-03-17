88895736af032fe0a46f8e93c4f975625557d974
Victor Imperial <VictorImperial2002@gmail.com>
2021-03-17 20:27:20 +0700

    Create Temperature.js

---
+++ Codegym (Lớp, Hướng đối tượng)/Class Temperature/Temperature.js
@@ -0,0 +1,16 @@
+class Temperature {
+    constructor (celcius) {
+    this.celcius = celcius;
+    }
+    getFahrenheit () {
+        return 1.8*this.celcius+32;
+    }
+    getKevin () {
+        return this.celcius+273;
+    }
+}
+let temperature = new Temperature(30);
+let fahrenheit = temperature.getFahrenheit();
+let kevin = temperature.getKevin();
+alert(fahrenheit);
+alert(fahrenheit);
